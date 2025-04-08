d17b16b8f14077088d70592622ec979c922b7625
housleyn <housleyn@byu.edu>
2025-04-08 16:23:37 -0600

    getting help

--- simple.py
+++ simple.py
@@ -14,6 +14,7 @@ class SIMPLE():
         self.alphau = .8
         self.iteration = 0
         self.tol = 1e-1
+        self.runtime = 0.0
 
     def generate_initial_guesses(self):
         mdot = 1
@@ -49,15 +50,16 @@ class SIMPLE():
 
     def calculate_p_prime(self):
         self.mesh.nodes[0].b = 0
-        self.mesh.nodes[-1].b = 0
+        # self.mesh.nodes[-1].b = 0
         for i in range(1,self.nx-1):
-            Fw = self.rho*self.mesh.u_faces[i-1].u*self.mesh.u_faces[i-1].area
-            Fe = self.rho*self.mesh.u_faces[i].u*self.mesh.u_faces[i].area
+            Fw = self.rho * self.mesh.u_faces[i-1].u * self.mesh.u_faces[i-1].area
+            Fe = self.rho * self.mesh.u_faces[i].u * self.mesh.u_faces[i].area
             Aw = self.mesh.u_faces[i-1].area
             Ae = self.mesh.u_faces[i].area
             dw = self.mesh.u_faces[i-1].d
             de = self.mesh.u_faces[i].d
             self.mesh.nodes[i].calculate_coefficients(self.rho, Fw, Fe, Aw, Ae, dw, de)
+
         A, b = self.mesh.construct_A_matrix_from_nodes()
         p_prime_solutions = np.linalg.solve(A, b)
         for i, p_prime_value in enumerate(p_prime_solutions):
@@ -72,7 +74,8 @@ class SIMPLE():
     def solve_for_corrected_velocities_and_pressures(self):
         for i in range(self.nx-1):
             self.mesh.u_faces[i].u_corr = self.mesh.u_faces[i].u + self.mesh.u_faces[i].u_prime
-        for i in range(self.nx):
+        self.mesh.nodes[0].p_corr = self.mesh.nodes[0].p - .5 * self.rho* (self.mesh.u_faces[0].u_corr * self.mesh.u_faces[0].area / self.mesh.nodes[0].area)**2 
+        for i in range(1,self.nx):
             self.mesh.nodes[i].p_corr = self.mesh.nodes[i].p + self.mesh.nodes[i].p_prime
     
     def calculate_new_pressures_and_velocities(self):
@@ -80,28 +83,36 @@ class SIMPLE():
             # self.mesh.u_faces[i].u_old = self.mesh.u_faces[i].u
             self.mesh.u_faces[i].u = (1-self.alphau)*self.mesh.u_faces[i].u_old + self.alphau*self.mesh.u_faces[i].u_corr
         for i in range(self.nx):
-            # self.mesh.nodes[i].p_old = self.mesh.nodes[i].p
+            self.mesh.nodes[i].p_old = self.mesh.nodes[i].p
             self.mesh.nodes[i].p = (1-self.alphap)*self.mesh.nodes[i].p_old + self.alphap*self.mesh.nodes[i].p_corr
 
 
-    def run(self):
-        self.generate_initial_guesses()
+    def set_old_to_new(self):
+        for i in range(self.nx-1):
+            self.mesh.u_faces[i].u_old = self.mesh.u_faces[i].u
+
+
+    def run(self, runtime):
+        if self.iteration == 0:
+            self.generate_initial_guesses()
         converged = False
-        for iteration in range(1000):
+        for iteration in range(runtime):
             self.calculate_u_new()
             self.calculate_p_prime()
             self.solve_for_new_u_prime()
             self.solve_for_corrected_velocities_and_pressures()
             self.calculate_new_pressures_and_velocities()
+            self.set_old_to_new()
             self.iteration += 1
             print(f"Iteration {self.iteration}:")
             if any(abs(self.mesh.u_faces[i].u - self.mesh.u_faces[i].u_old) < self.tol for i in range(self.nx-1)) and \
                any(abs(self.mesh.nodes[i].p - self.mesh.nodes[i].p_old) < self.tol for i in range(self.nx)) :
                 converged = True
-        self.plot_pressure_vs_distance()
+        # self.plot_pressure_vs_distance()
 
     def plot_pressure_vs_distance(self):
         # Extract the position and pressure values from the nodes
+        
         positions = np.array([node.position for node in self.mesh.nodes])
         pressures = np.array([node.p for node in self.mesh.nodes])
 
